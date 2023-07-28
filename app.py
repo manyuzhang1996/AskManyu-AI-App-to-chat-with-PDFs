a76c53eee6df98494a59eebaa5c2b303b38abcd7
Emma's <emmazhang@Emma.attlocal.net>
2023-07-28 11:44:46 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,126 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    # loop through all pdfs
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        # loop through all pages
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text) # return as a list
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # use Instructor Embedding model
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask Manyu",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    # initialize the conversation and history every time
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Ask Manyu anything about your PDFs :books:")
+    st.write("👋 Hello! Welcome to ***Ask Manyu***, an AI application created by **Manyu Zhang**, an AI Engineer.")
+    st.write("I was built with technologies such as OpenAI API, LangChain, Streamlit, and more. And I'm here to assist you with any questions you have about your PDF documents.")
+    st.write("If you have any questions or need assistance, don't hesitate to ask. Enjoy your experience with Ask Manyu!")
+    st.markdown("---")
+
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    # design the sidebar
+    with st.sidebar:
+        st.subheader("Your PDF documents")
+        # allow user to upload multiple pdf files, return a list of pdfs
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        
+        if st.button("Process"):
+            # add a spinner to show the processing status
+            with st.spinner("Processing"):
+                # 1. get the pdf texts
+                raw_text = get_pdf_text(pdf_docs)
+
+                # 2. split the texts into chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # 3. create vector store with embeddings
+                vectorstore = get_vectorstore(text_chunks)
+
+                # 4. create conversation chain object
+                # use session_state to ensure conversation persistency, we can call the conversations outside of the process part
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+    
+    # Add a separator line
+        st.markdown("---")
+
+    # Add the contact information in the sidebar
+    with st.sidebar:
+        st.subheader("Contact Me")
+        st.write("🔗 LinkedIn: [Manyu Zhang's LinkedIn](https://www.linkedin.com/in/manyu-zhang-b98457186/)")
+        st.write("📧 Email: zhangmanyuzmy@gmail.com")
+        st.write("🐙 GitHub: [Manyu Zhang's GitHub](https://github.com/manyuzhang1996)")
+        
+
+if __name__ == '__main__':
+    main()
---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.postimg.cc/d36r3fF7/CV-phone-1-M.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://icones.pro/wp-content/uploads/2021/03/avatar-de-personne-icone-femme.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
